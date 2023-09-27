785d5955546063f6db81e2e5d40ebd91e406b8b0
Илья Афанасьев <iafanasevmail@gmail.com>
2023-09-28 00:54:01 +0300

    change serializers and views

--- downloader/api/urls.py
+++ downloader/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import FileViewSet
-
-router = DefaultRouter()
-router.register('files', FileViewSet, basename='files')
+from api.views import FileAPIView
 
 urlpatterns = [
-    path('upload/', include(router.urls), name='upload'),
+    path('upload/', FileAPIView.as_view(), name='upload'),
+
+# router = DefaultRouter()
+# router.register('files', FileViewSet, basename='files')
+
+# urlpatterns = [
+#     path('upload/', include(router.urls), name='upload'),
 ]
--- downloader/api/views.py
+++ downloader/api/views.py
@@ -1,10 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.serializers import FileSerializer
-from files.models import File
 
 
-
-class FileViewSet(viewsets.ModelViewSet):
-    queryset = File.objects.all().order_by('-uploaded_at')
+class FileAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
     serializer_class = FileSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
